46c6c9386d962fe302ce51f00e07f1d8218e4d36
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-04-24 12:23:40 +0200

    refactor: generalize

--- tests.js
+++ tests.js
@@ -4,7 +4,7 @@ context = describe;
 function containsTabuWord(recognizedSentence, tabuWords){
     var sentence_array = recognizedSentence.split(" ");
     var i;
-    for (i = 0; i < 3; i++){
+    for (i = 0; i < sentence_array.length; i++){
         if (tabuWords.indexOf(sentence_array[i]) > -1) return true;
     }    
     return false; 
