3a0fde27c7458e0bb2aeff1a097394743e32bd43
Lorena Fernández Martínez <lorena.fernandez@me.com>
2015-04-24 17:22:23 +0200

    Deleting duplicate files

--- public/test/validator.js
+++
@@ -1,8 +0,0 @@
-function containsTabuWord(recognizedSentence, tabuWords){
-    var sentence_array = recognizedSentence.split(" ");
-    var i;
-    for (i = 0; i < sentence_array.length; i++){
-        if (tabuWords.indexOf(sentence_array[i]) > -1) return true;
-    }    
-    return false; 
-}
